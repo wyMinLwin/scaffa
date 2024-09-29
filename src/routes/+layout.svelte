adce1bea44639e3d29c025772e506470f61b0c1c
wyMinLwin <waiyanminlwin421@gmail.com>
2024-09-29 20:57:22 +0700

    :tada: Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="app font-display">
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div class="text-2xl">Frontend Makro</div>
+</section>
+
+<style>
+	
+</style>
