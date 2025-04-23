73b0eb30b2702e67ac9510197fb9a11a85073535
wyMinLwin <waiyanminlwin421@gmail.com>
2025-04-23 21:39:47 +0700

    :heavy_plus_sign: Add Shadcn for Vute Template

--- packages/create-makro/templates/vue-std/vite.config.ts
+++ packages/create-makro/templates/vue-std/vite.config.ts
@@ -1,9 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
