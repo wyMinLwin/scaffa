c14f04a641d0adf2ded9a4cf39f5b7ebf130385d
wyMinLwin <waiyanminlwin421@gmail.com>
2025-04-25 21:42:16 +0700

    :sparkles: Vue Template Usage for TanStack

--- packages/create-makro/templates/vue-std/src/api/products/types.d.ts
+++ packages/create-makro/templates/vue-std/src/api/products/types.d.ts
@@ -17,3 +17,22 @@ type ProductType = {
     depth: number
   }
 }
+
+type CreateProductType = {
+  title: string
+  description: string
+  category: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  tags: string[]
+  brand: string
+  sku: string
+  weight: number
+  dimensions: {
+    width: number
+    height: number
+    depth: number
+  }
+}
