c0db477238aa0f55196d87c05e8e09325d52124d
wyMinLwin <waiyanminlwin421@gmail.com>
2026-01-08 23:27:42 +0700

    chore: code removal of support-scaffa

--- packages/support-scaffa/bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { validate } from '../lib/validator.js';
-
-// console.log('Hello from support-scaffa!');
-// console.log(process.env.npm_lifecycle_event);
-
-await validate();
